da0fa9d25f411df7a232430472b76885cf1eb3fc
Amit571 <amitshinde44020@gmail.com>
2025-12-12 21:52:47 +0530

    Git Debug True

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-from sqlalchemy import false, true
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost/form'
-# db = SQLAlchemy(app)
 
-
-# class Datatabel(db.Model):
-#     name = db.Column(db.String, primary_key=false)
-#     mobile = db.Column(db.Integer(), unique=false)
-#     email = db.Column(db.String(), unique=false)
-
+# Initialize chatbot
 ait_bot = ChatBot("Chatterbot")
 trainer = ChatterBotCorpusTrainer(ait_bot)
-trainer.train("data/data.yml")
+trainer.train('data/data.yml')  # Ensure a valid corpus
 
-
-@app.route("/", methods = ['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    # if (request.method == 'POST'):
-    #     name = request.form.get('name')
-    #     mobile = request.form.get('mobile')
-    #     email = request.form.get('email')
-    #     entry = Datatabel(name = name, mobile = mobile, email = email )
-    #     db.session.add(entry)
-    #     db.session.commit()
     return render_template("index.html")
 
 @app.route("/get")
@@ -35,6 +18,5 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(ait_bot.get_response(userText))
 
-
 if __name__ == "__main__":
-    app.run(debug=true)
+    app.run(debug=True)  # Fix boolean value
